816720343c5f070eafaf7f8985ca67ee2e46bcbf
Andrew Lin <andrewlin16@gmail.com>
2015-04-20 03:41:03 -0500

    Add some Packet operators for read/write
    Can read and write int, float, Vector3, and Quaternion.

--- Packet.cpp
+++ Packet.cpp
@@ -1,8 +1,31 @@
+#include <cstring>
 #include "Packet.h"
 
-Packet::Packet() : length(0), buffer() {}
+//---------------------------------------------------------------------------
 
-Packet::Packet(char* buf, int len) : length(len), buffer(buf, buf + len) {}
+void Packet::mem_write_to_packet(const void* data, const int size) {
+    const char* buf = (const char*) data;
+
+    buffer.insert(buffer.end(), buf, buf + size);
+    length += size;
+}
+
+void Packet::mem_read_from_packet(void* data, const int size) {
+    memcpy(data, &buffer[position], size);
+    position += size;
+}
+
+//---------------------------------------------------------------------------
+
+Packet::Packet() :
+    buffer(),
+    length(0),
+    position(0) {}
+
+Packet::Packet(char* buf, int len) :
+    buffer(buf, buf + len),
+    length(len),
+    position(0) {}
 
 int Packet::size() const {
     return length;
@@ -11,3 +34,45 @@ int Packet::size() const {
 const char* Packet::data() const {
     return &buffer[0];
 }
+
+//---------------------------------------------------------------------------
+
+Packet& Packet::operator<<(const int a) {
+    mem_write_to_packet(&a, sizeof(int));
+    return *this;
+}
+
+Packet& Packet::operator<<(const float a) {
+    mem_write_to_packet(&a, sizeof(float));
+    return *this;
+}
+
+Packet& Packet::operator<<(const Ogre::Vector3& a) {
+    return *this << a.x << a.y << a.z;
+}
+
+Packet& Packet::operator<<(const Ogre::Quaternion& a) {
+    return *this << a.w << a.x << a.y << a.z;
+}
+
+//---------------------------------------------------------------------------
+
+Packet& Packet::operator>>(int &a) {
+    mem_read_from_packet(&a, sizeof(int));
+    return *this;
+}
+
+Packet& Packet::operator>>(float &a) {
+    mem_read_from_packet(&a, sizeof(float));
+    return *this;
+}
+
+Packet& Packet::operator>>(Ogre::Vector3& a) {
+    return *this >> a.x >> a.y >> a.z;
+}
+
+Packet& Packet::operator>>(Ogre::Quaternion& a) {
+    return *this >> a.w >> a.x >> a.y >> a.z;
+}
+
+//---------------------------------------------------------------------------
--- Packet.h
+++ Packet.h
@@ -1,12 +1,17 @@
 #ifndef PACKET_H_
 #define PACKET_H_
 
+#include <Ogre.h>
 #include <vector>
 
 class Packet {
     private:
-        int length;
         std::vector<char> buffer;
+        int length;
+        int position;
+
+        void mem_write_to_packet(const void* data, const int size);
+        void mem_read_from_packet(void* data, const int size);
 
     public:
         Packet();
@@ -14,6 +19,16 @@ class Packet {
 
         int size() const;
         const char* data() const;
+
+        Packet& operator<<(const int a);
+        Packet& operator<<(const float a);
+        Packet& operator<<(const Ogre::Vector3& a);
+        Packet& operator<<(const Ogre::Quaternion& a);
+
+        Packet& operator>>(int& a);
+        Packet& operator>>(float& a);
+        Packet& operator>>(Ogre::Vector3& a);
+        Packet& operator>>(Ogre::Quaternion& a);
 };
 
 #endif /* PACKET_H_ */
