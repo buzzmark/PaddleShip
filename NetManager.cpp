6f7f5c1abf57b74932a53736e581ccaff914049d
Andrew Lin <andrewlin16@gmail.com>
2015-05-12 17:13:43 -0500

    Add UDP support to NetManager along TCP

--- NetManager.cpp
+++ NetManager.cpp
@@ -5,20 +5,32 @@
 NetManager::NetManager(){
     SDLNet_Init();
     socket_set = SDLNet_AllocSocketSet(16);
+    udp_recv_packet = SDLNet_AllocPacket(MAX_RECV_PACKET_SIZE);
     nextClientId = 1;
     isRunning = false;
 }
 
 NetManager::~NetManager(){
     SDLNet_FreeSocketSet(socket_set);
+    SDLNet_FreePacket(udp_recv_packet);
 
     if (isRunning) {
-        SDLNet_TCP_Close(server_tcp);
+        if (isServer) {
+            for (auto client : clients_udp) {
+                int channel = client.second;
+                SDLNet_UDP_Unbind(server_udp, channel);
+            }
+        } else {
+            SDLNet_UDP_Unbind(server_udp, 0);
+        }
 
         for (auto client : clients_tcp) {
             TCPsocket& socket = client.second;
             SDLNet_TCP_Close(socket);
         }
+
+        SDLNet_UDP_Close(server_udp);
+        SDLNet_TCP_Close(server_tcp);
     }
 
     SDLNet_Quit();
@@ -50,6 +62,13 @@ void NetManager::startServer(){
         exit(EXIT_FAILURE);
     }
 
+    /* Open UDP socket */
+    if (!(server_udp = SDLNet_UDP_Open(49152)))
+    {
+        fprintf(stderr, "SDLNet_UDP_Open: %s\n", SDLNet_GetError());
+        exit(EXIT_FAILURE);
+    }
+
     isRunning = true;
 }
 
@@ -62,6 +81,7 @@ void NetManager::startClient(char* host) {
         exit(EXIT_FAILURE);
     }
 
+    /* Open TCP connection to server */
     if (!(server_tcp = SDLNet_TCP_Open(&ip)))
     {
         fprintf(stderr, "SDLNet_TCP_Open: %s\n", SDLNet_GetError());
@@ -71,6 +91,18 @@ void NetManager::startClient(char* host) {
     if (SDLNet_AddSocket(socket_set, (SDLNet_GenericSocket) server_tcp) < 0)
     {
         fprintf(stderr, "SDLNet_AddSocket: %s\n", SDLNet_GetError());
+        exit(EXIT_FAILURE);
+    }
+
+    /* Bind UDP channel to server */
+    if (!(server_udp = SDLNet_UDP_Open(49152)))
+    {
+        fprintf(stderr, "SDLNet_UDP_Open: %s\n", SDLNet_GetError());
+        exit(EXIT_FAILURE);
+    }
+
+    if (SDLNet_UDP_Bind(server_udp, 0, &ip) == -1) {
+        fprintf(stderr, "SDLNet_UDP_Bind: %s\n", SDLNet_GetError());
         exit(EXIT_FAILURE);
     }
 
@@ -106,6 +138,7 @@ std::unordered_map<int, TCPsocket>::iterator NetManager::messageSingleClientTCP(
     if (SDLNet_TCP_Send(client, buf, len) < len) {
         queuedDisconnects.push_back(iter->first);
 
+        SDLNet_UDP_Unbind(server_udp, clients_udp[iter->first]);
         SDLNet_TCP_Close(client);
         SDLNet_DelSocket(socket_set, (SDLNet_GenericSocket) client);
         return clients_tcp.erase(iter);
@@ -130,6 +163,47 @@ void NetManager::messageClientTCP(int clientId, const Packet &p) {
     }
 }
 
+UDPpacket* createUdpPacket(const Packet& p) {
+    const char* buf = p.data();
+    const int len = p.size();
+
+    UDPpacket* send_packet = SDLNet_AllocPacket(len);
+    memcpy(send_packet->data, buf, len);
+    send_packet->len = len;
+
+    return send_packet;
+}
+
+void NetManager::messageServerUDP(const Packet &p) {
+    UDPpacket* send_packet = createUdpPacket(p);
+    SDLNet_UDP_Send(server_udp, 0, send_packet);
+    SDLNet_FreePacket(send_packet);
+}
+
+void NetManager::messageClientsUDP(const Packet& p) {
+    UDPpacket* send_packet = createUdpPacket(p);
+
+    for (auto client : clients_udp) {
+        SDLNet_UDP_Send(server_udp, client.second, send_packet);
+    }
+
+    SDLNet_FreePacket(send_packet);
+}
+
+void NetManager::messageClientUDP(int clientId, const Packet& p) {
+    auto iter = clients_udp.find(clientId);
+
+    if (iter != clients_udp.end()) {
+        UDPpacket* send_packet = createUdpPacket(p);
+        SDLNet_UDP_Send(server_udp, iter->second, send_packet);
+        SDLNet_FreePacket(send_packet);
+    }
+}
+
+//------------------------------------------------------------
+// Update fuctions
+//------------------------------------------------------------
+
 NetUpdate NetManager::checkForUpdates() {
     SDLNet_CheckSockets(socket_set, 0);
 
@@ -147,6 +221,14 @@ NetUpdate NetManager::checkForUpdates() {
 
                 clients_tcp[nextClientId] = connection;
                 SDLNet_AddSocket(socket_set, (SDLNet_GenericSocket) connection);
+
+                IPaddress* client_ip = SDLNet_TCP_GetPeerAddress(connection);
+                int channel = SDLNet_UDP_Bind(server_udp, -1, client_ip);
+                if (channel != -1) {
+                    clients_udp[nextClientId] = channel;
+                    udp_channels[channel] = nextClientId;
+                }
+
                 nextClientId++;
             }
         }
@@ -180,7 +262,7 @@ bool read_buffer(TCPsocket socket, void* vbuf, int len) {
 void NetManager::serverGetData(NetUpdate& update) {
     std::unordered_map<int, Packet>& data = update.data;
 
-    // check for client messages
+    // check for client messages (TCP)
     auto iter = clients_tcp.begin();
 
     while (iter != clients_tcp.end()) {
@@ -197,6 +279,7 @@ void NetManager::serverGetData(NetUpdate& update) {
                 } else {
                     update.disconnects.push_back(iter->first);
 
+                    SDLNet_UDP_Unbind(server_udp, clients_udp[iter->first]);
                     SDLNet_TCP_Close(client);
                     SDLNet_DelSocket(socket_set, (SDLNet_GenericSocket) client);
                     iter = clients_tcp.erase(iter);
@@ -206,6 +289,7 @@ void NetManager::serverGetData(NetUpdate& update) {
             } else {
                 update.disconnects.push_back(iter->first);
 
+                SDLNet_UDP_Unbind(server_udp, clients_udp[iter->first]);
                 SDLNet_TCP_Close(client);
                 SDLNet_DelSocket(socket_set, (SDLNet_GenericSocket) client);
                 iter = clients_tcp.erase(iter);
@@ -214,12 +298,39 @@ void NetManager::serverGetData(NetUpdate& update) {
             ++iter;
         }
     }
+
+    // check for client messages (UDP)
+    if (SDLNet_UDP_Recv(server_udp, udp_recv_packet) == 1) {
+        int channel = udp_recv_packet->channel;
+        if (channel != -1) {
+            int clientId = udp_channels[channel];
+            Packet p((char*) udp_recv_packet->data, udp_recv_packet->len);
+
+            if (data.find(clientId) != data.end()) {
+                queuedUdpUpdates[clientId].push(p);
+            } else {
+                data[clientId] = p;
+            }
+        }
+    }
+
+    // check for queued client messages from UDP
+    for (auto client : queuedUdpUpdates) {
+        int clientId = client.first;
+        std::queue<Packet>& udp_queue = client.second;
+
+        if (!udp_queue.empty() && data.find(clientId) == data.end()) {
+            data[clientId] = udp_queue.front();
+            udp_queue.pop();
+        }
+    }
 }
 
 void NetManager::clientGetData(NetUpdate& update) {
     std::unordered_map<int, Packet>& data = update.data;
 
     if (SDLNet_SocketReady(server_tcp)) {
+        // read in server message (TCP)
         int len;
 
         if (read_buffer(server_tcp, &len, sizeof(int))) {
@@ -232,6 +343,13 @@ void NetManager::clientGetData(NetUpdate& update) {
             delete[] buf;
         } else {
             update.disconnects.push_back(0);
+        }
+    } else {
+        // check for server message (UDP)
+        if (SDLNet_UDP_Recv(server_udp, udp_recv_packet) == 1) {
+            if (udp_recv_packet->channel != -1) {
+                data[0] = Packet((char*) udp_recv_packet->data, udp_recv_packet->len);
+            }
         }
     }
 }
--- NetManager.h
+++ NetManager.h
@@ -6,18 +6,25 @@
 #include "Packet.h"
 #include "NetUpdate.h"
 
+#define MAX_RECV_PACKET_SIZE 8192
+
 class NetManager
 {
     private:
         IPaddress ip;
         TCPsocket server_tcp;
+        UDPsocket server_udp;
         std::unordered_map<int, TCPsocket> clients_tcp;
+        std::unordered_map<int, int> clients_udp;
+        std::unordered_map<int, int> udp_channels;
         SDLNet_SocketSet socket_set;
+        UDPpacket* udp_recv_packet;
         bool isServer;
         bool isRunning;
         int nextClientId;
 
         std::vector<int> queuedDisconnects;
+        std::unordered_map<int, std::queue<Packet>> queuedUdpUpdates;
 
         std::unordered_map<int, TCPsocket>::iterator messageSingleClientTCP(std::unordered_map<int, TCPsocket>::iterator iter, const Packet &p);
 
@@ -35,6 +42,10 @@ class NetManager
         void messageServerTCP(const Packet& p);
         void messageClientsTCP(const Packet& p);
         void messageClientTCP(int clientId, const Packet& p);
+        void messageServerUDP(const Packet& p);
+        void messageClientsUDP(const Packet& p);
+        void messageClientUDP(int clientId, const Packet& p);
+
         NetUpdate checkForUpdates();
 };
 
