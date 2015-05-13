fb60ee35ab59c5aba9ba3daec810b30ac069d366
Andrew Lin <andrewlin16@gmail.com>
2015-05-12 20:53:06 -0500

    [test] Send in nettest

--- NetTest.cpp
+++ NetTest.cpp
@@ -5,6 +5,7 @@
 #include <chrono>
 #include <iostream>
 #include <thread>
+#include <string>
 
 int main(int argc, char** argv) {
     NetManager net;
@@ -24,7 +25,22 @@ int main(int argc, char** argv) {
         std::cout << "conn id: " << update.connectionId << std::endl;
         std::cout << "disconnects: " << update.disconnects.size() << std::endl;
         std::cout << "has server update: " << update.hasServerUpdate() << std::endl;
+        if (update.hasServerUpdate()) {
+            Packet& p = update.getServerUpdate();
+            std::string msg;
+            int t;
+
+            p >> msg >> t;
+
+            std::cout << "update: " << msg << " (@" << t << ")" << std::endl;
+        }
         std::cout << std::endl;
+
+        if (net.numClients() > 0) {
+            Packet p;
+            p << "Hello" << tick;
+            net.messageClientsUDP(p);
+        }
 
         std::this_thread::sleep_for(std::chrono::seconds(1));
         tick++;
